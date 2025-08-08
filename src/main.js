88b456c9882e98bf50ae486d463fa9b5771fd951
teehee574 <digovetsmaxim@gmail.com>
2025-08-08 14:42:15 +0200

    add scss

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import './styles.scss';
+
 
 document.querySelector('#app').innerHTML = `
   <div>
